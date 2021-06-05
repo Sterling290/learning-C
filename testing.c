d03ccf9922c8821bd6fc595c4e2dc89fe741fabd
Sterling290 <sterling.becvar@gmail.com>
2021-06-05 17:03:59 -0600

    cool

--- testing.c
+++ testing.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
-int main(){
-cout << "sick" ;
-return 0;
-};
+using namespace std;
+
+int main() {
+cout << "Hello World!";
+ string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+ string sick[3] = {"cool","3","4"};
+  cout << "\n I am learning C++";
+  return 0;
+}
