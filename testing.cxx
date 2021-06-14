2d544dd3dbefb66753e1678bc8dbce23254f48de
Sterling290 <sterling.becvar@gmail.com>
2021-06-13 23:28:37 -0600

    Trying to make array board

--- testing.cxx
+++ testing.cxx
@@ -1,57 +1,66 @@
 #include <iomanip>
+//#include <../include/color.hpp>
 #include <iostream>
+//#include "emoji.h"
+#include <fstream>
 #include <stdlib.h>
 #include <unistd.h>
-#include <term.h>
+#include <limits>
+#include <string>
 
 using namespace std;
 
 int hours = 0;
 int minutes = 0;
 int seconds = 0;
+string trying =  "\x1B[31mTexting\033[0m\t\t";
+string array1[5] = {"|","0","0","0","|"};
+string array2[5] = {"|","0","0","0","|"};
+string array3[5] = {"|","0","0","0","|"};
+string array4[5] = {"|","0","0","0","|"};
+string array5[5] = {"|","0","0","0","|"};
+
   
 // function to display the timer
 void displayClock()
 {
+    //HANDLE hConsole = GetStdHandle("STD_OUTPUT_HANDLE");
     // system call to clear the screen
     system("clear");
+    system("Color 1A");
     //<term.h clear
-  putp( tigetstr( "clear" ) );
   
-    
-    cout << setfill(' ') << setw(55) << "         TIMER         \n" ;
-    cout << setfill(' ') << setw(55) << " --------------------------\n";
+  //SetConsoleTextAttribute(hConsole, 2);
+    cout << string(10, '\n');
+     std::cout << "Hello, what is your name?" << std::endl;
+     cout << setfill(' ') << setw(55) <<"         TIMER         \n";
+    cout << setfill(' ') << setw(55) << "-----------" << array1[4]<<"---------------\n";
     cout << setfill(' ') << setw(29);
     cout << "| " << setfill('0') << setw(2) << hours << " hrs | ";
     cout << setfill('0') << setw(2) << minutes << " min | ";
     cout << setfill('0') << setw(2) << seconds << " sec |" << endl;
-    cout << setfill(' ') << setw(55) << " --------------------------\n";
-    system ("CLS");
+    cout << setfill(' ') << setw(55) << "--------------------------\n";
+  
 }
   
 void timer()
 {
-    // infinte loop because timer will keep 
-    // counting. To kill the process press
-    // Ctrl+D. If it does not work ask
-    // ubuntu for other ways.
     while (true) {
           
         // display the timer
         displayClock();
-  
         // sleep system call to sleep 
         // for 1 second
         sleep(1);
   
         // increment seconds
         seconds++;
-  
-        // if seconds reaches 60
+        // if seconds reaches 11
         if (seconds == 11) {
+            cout << trying;  
           return ;
             // increment minutes
-            cout << "done";  
+            
         }
     }
 }
